8640b5e7cb5a848f143d32baec1b74920da4b58b
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-12-04 09:37:40 -0600

    Test commit for fetching blogs

--- blog.js
+++ blog.js
@@ -1,5 +1,20 @@
 window.onload = load()
 
-function load() {
-    
+
+
+async function load() {
+    const posts = [
+        `Blog/Filler.html`,
+        `Blog/Test1.html`,
+        `Blog/Test2.html`
+    ];
+    for (const post of posts) {
+        console.log(post)
+        const response = await fetch(post);
+        const html = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        console.log(doc.getElementById("test").innerHTML);
+        console.log("")
+    }
 }
