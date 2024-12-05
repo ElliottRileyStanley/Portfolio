0376c1b2e524427322644daa931f776bea50f09b
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-12-05 09:44:40 -0600

    Loading posts functioning

--- blog.js
+++ blog.js
@@ -6,15 +6,21 @@ async function load() {
     const posts = [
         `/Blog/Filler.html`,
         `/Blog/Test1.html`,
-        `/Blog/Test2.html`
+        `/Blog/Test2.html`,
+        `/Blog/Test2 copy.html`
     ];
     for (const post of posts) {
-        console.log(post)
         const response = await fetch(post, {method: 'GET'});
         const html = await response.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
-        alert(doc.documentElement.innerHTML);
+        var post_div = document.createElement('div')
+        post_div.innerHTML = `
+        <h1 class="postTitle">${doc.getElementsByName("title")[0].content}</h1>
+        <p class="postDescription">${doc.getElementsByName("description")[0].content}</p>
+        <p class="postDate">${doc.getElementsByName("date")[0].content}</p>
+        `
+        document.getElementById("posts").appendChild(post_div)
         console.log("")
     }
 }
