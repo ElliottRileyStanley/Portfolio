c5b4ef173157fe14fb00fbd1b238c77a161e015b
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-12-06 06:31:53 -0600

    Display language on blogs

--- blog.js
+++ blog.js
@@ -19,8 +19,7 @@ async function load() {
         var post_div = document.createElement('div');
         post_div.classList.add("post");
         post_div.innerHTML = `
-        <h1 class="postTitle">${doc.getElementsByName("title")[0].content}</h1>
-
+        <h1 class="postTitle">${doc.getElementsByName("title")[0].content}<img class="postLanguage" src="${doc.getElementsByName("language")[0].content}"></h1>
         <p class="postDescription">${doc.getElementsByName("description")[0].content}</p>
         <p class="postDate">${doc.getElementsByName("date")[0].content}</p>
         `;
